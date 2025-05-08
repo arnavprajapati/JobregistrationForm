224124594882fb3c41dbb589c7d99f2d88725ce9
Arnav <arnavprajapati3101@gmail.com>
2025-05-08 09:10:33 +0530

    ui to see data from database

---
+++ list.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>List of Candidates - Web Developer</title>
+  <link rel="stylesheet" href="list.css">
+</head>
+
+<body>
+  <div class="container">
+    <a href="index.html" class="back-btn">← Back to Form</a>
+    <h1>List of candidates for web developer job</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>NAME</th>
+          <th>DEGREE</th>
+          <th>MOBILE</th>
+          <th>EMAIL</th>
+          <th>REFER</th>
+          <th>POST</th>
+          <th>OPERATION</th>
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
+    </table>
+  </div>
+</body>
+
+</html>
